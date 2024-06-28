28c187fd4609846e4301d98f51112ce748bde27d
EzeNotebook <ezgarcia@frba.utn.edu.ar>
2024-06-27 22:51:41 -0300

    responsive en el navbar terminado con el css

---
+++ src/utils/listNavbar.ts
@@ -0,0 +1,30 @@
+import type { NavbarMenu } from "../interface";
+
+export const listNavbar: NavbarMenu[] = [
+    {
+      icon: "/svg/header-design.svg",
+      href: "/design",
+      name: "design",
+    },
+    {
+      icon: "/svg/header-code.svg",
+      href: "/code",
+      name: "code",
+    },
+    {
+      icon: "/svg/header-collection.svg",
+      href: "/collenctions",
+      name: "collenctions",
+    },
+    {
+      icon: "/svg/header-about.svg",
+      href: "/about",
+      name: "about",
+    },
+    {
+      icon: "/svg/header-contact.svg",
+      href: "/contact",
+      name: "contact",
+    },
+  ];
+  
