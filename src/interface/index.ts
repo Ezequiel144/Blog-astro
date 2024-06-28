879d7335158fc55814713f7187bb54f78cd2d65e
EzeNotebook <ezgarcia@frba.utn.edu.ar>
2024-06-27 21:16:30 -0300

    terminada modificacion de cards

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -2,3 +2,4 @@ export * from "./cardPostNow.interface";
 export * from "./tagCard.interface";
 export * from "./navbarMenu.interface";
 export * from "./menuFilter";
+export * from "./tagCard.interface";
